9d020651312dd59f2bd06d859c65b1a11fcdb0b2
Shubham Bansal <159136423+Shubham-py404@users.noreply.github.com>
2024-08-24 12:17:52 +0530

    Add files via upload

--- JavaScript/17_SPOTIFY/script.js
+++ JavaScript/17_SPOTIFY/script.js
@@ -1,1 +1,43 @@
 console.log("hello from java script");
+  
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:5500/17_SPOTIFY/audio/")
+    let response = await a.text();
+    console.log(response);
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as= div.getElementsByTagName("a")
+    console.log(as);
+    let songs = []
+    for (let i = 0; i < as.length; i++) {
+        const element = as[i];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href)
+        }
+    }
+    return  songs
+     
+}
+async function main(params) {
+// get the list of all the songs
+    let songs = await getSongs()
+    console.log(songs);
+    
+    let songUl = document.querySelector(".songlist").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUl.innerHTML = songUl.innerHTML + song;
+    }
+    //playing the songs
+    var audio = new Audio(songs[3])
+    // audio.play();
+
+    audio.addEventListener("loadeddata",()=>{
+    let duration= audio.duration;
+    console.log(audio.duration, audio.currentSrc, audio.currentTime);
+    
+   })
+}
+main() 
+
+
+
